2bf4b94dc55a6168bb4f75e15c943fcfde6549b4
江纬 <wei@algmon.com>
2023-08-29 22:28:47 +0800

    Refactor the project structure

--- script/copy_rename_files.py
+++ script/copy_rename_files.py
@@ -1,9 +1,9 @@
 import shutil
 import os
 
-source_file = "../kb/structured/domian.math.grade.5.上/练习二.qa.csv"
-destination_folder = "../kb/structured/domian.math.grade.5.上/"
+source_file = "../kb/structured/domain.math.grade.5.上/old/练习5.qa.csv"
+destination_folder = "../kb/structured/domain.math.grade.5.上/old/"
 
-for i in range(3, 25):
+for i in range(6, 25):
     destination_file = f"练习{i}.qa.csv"
     shutil.copy(source_file, os.path.join(destination_folder, destination_file))
