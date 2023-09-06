daf5af6dd8de7612f1abe23ca40bafe283795095
江纬 <wei@algmon.com>
2023-09-06 10:30:21 +0800

    Update 02.六年级数学上习题总结表 & expand targeted testing llms

--- script/copy_rename_files.py
+++ script/copy_rename_files.py
@@ -1,9 +1,13 @@
+"""
+COPY and RENAME files for repetition
+"""
 import shutil
 import os
 
-source_file = "../kb/structured/domain.math.grade.5.下/练习1.qa.csv"
-destination_folder = "../kb/structured/domain.math.grade.5.下/"
+source_file = "../kb/structured/domain.教培/domain.math.grade.6/ex.1.qa.csv"
+destination_folder = "../kb/structured/domain.教培/domain.math.grade.6/"
 
-for i in range(2, 28):
-    destination_file = f"练习{i}.qa.csv"
+for i in range(2, 24):
+    destination_file = f"ex.{i}.qa.csv"
+    print(destination_file)
     shutil.copy(source_file, os.path.join(destination_folder, destination_file))
