f616c454b42051a255dfe1702bbb13c8ef815b2d
ImpactDi <2197943267@qq.com>
2023-09-11 18:49:57 +0800

    笔试题目

---
+++ Agentproject.py
@@ -0,0 +1,161 @@
+class Multimodal_models:
+    def image_processing(self, image):
+        # 在图片上进行思考和处理
+        Thought_process = "在图片上进行思考：分析图像特征，识别物体。"
+        return Thought_process
+
+    def data_processing(self, data):
+        # 在数值上进行思考和处理
+        Thought_process = "在数值上进行思考：进行数值计算和分析。"
+        return Thought_process
+
+    def text_processing(self, text):
+        # 在文本上进行思考和处理
+        Thought_process = "在文本上进行思考：分析文本内容，提取关键信息。"
+        return Thought_process
+
+    def symbol_handling(self, symbol):
+        # 在符号上进行思考和处理
+        Thought_process = "在符号上进行思考：进行符号运算和推理。"
+        return Thought_process
+class Brain:
+    def __init__(self, models):
+        self.models = models
+
+    def ponder(self, enter, Multimodal):
+        # 使用多模态大模型进行思考
+        if Multimodal == "图片":
+            Thought_process = self.models.image_processing(enter)
+        elif Multimodal == "数值":
+            Thought_process = self.models.data_processing(enter)
+        elif Multimodal == "文本":
+            Thought_process = self.models.text_processing(enter)
+        elif Multimodal == "符号":
+            Thought_process = self.models.symbol_handling(enter)
+        else:
+            Thought_process = "未知模态"
+        print("大脑思考过程:", Thought_process)
+        return Thought_process
+
+class A:
+    def picture_action(self, Thought_process):
+        # 使用四肢A来实现图片处理动作
+        Action = "使用A来实现图片处理：" + Thought_process
+        print(Action)
+    def text_changes(self, Thought_process):
+        # 使用B来实现文本处理动作
+        Action = "使用A来实现文本处理：" + Thought_process
+        print(Action)
+    def value_change(self, Thought_process):
+        # 使用C来实现数值处理动作
+        Action = "使用A来实现数值处理：" + Thought_process
+        print(Action)
+    def symbol_operations(self, Thought_process):
+        # 使用D来实现符号处理动作
+        Action = "使用A来实现符号处理：" + Thought_process
+        print(Action)
+
+class B:
+    def picture_action(self, Thought_process):
+        # 使用四肢A来实现图片处理动作
+        Action = "使用B来实现图片处理：" + Thought_process
+        print(Action)
+    def text_changes(self, Thought_process):
+        # 使用B来实现文本处理动作
+        Action = "使用B来实现文本处理：" + Thought_process
+        print(Action)
+    def value_change(self, Thought_process):
+        # 使用C来实现数值处理动作
+        Action = "使用B来实现数值处理：" + Thought_process
+        print(Action)
+    def symbol_operations(self, Thought_process):
+        # 使用D来实现符号处理动作
+        Action = "使用B来实现符号处理：" + Thought_process
+        print(Action)
+class C:
+    def picture_action(self, Thought_process):
+        # 使用四肢A来实现图片处理动作
+        Action = "使用C来实现图片处理：" + Thought_process
+        print(Action)
+    def text_changes(self, Thought_process):
+        # 使用B来实现文本处理动作
+        Action = "使用C来实现文本处理：" + Thought_process
+        print(Action)
+    def value_change(self, Thought_process):
+        # 使用C来实现数值处理动作
+        Action = "使用C来实现数值处理：" + Thought_process
+        print(Action)
+
+    def symbol_operations(self, Thought_process):
+        # 使用D来实现符号处理动作
+        Action = "使用C来实现符号处理：" + Thought_process
+        print(Action)
+class D:
+    def picture_action(self, Thought_process):
+        # 使用四肢A来实现图片处理动作
+        Action = "使用D来实现图片处理：" + Thought_process
+        print(Action)
+    def text_changes(self, Thought_process):
+        # 使用B来实现文本处理动作
+        Action = "使用D来实现文本处理：" + Thought_process
+        print(Action)
+    def value_change(self, Thought_process):
+        # 使用C来实现数值处理动作
+        Action = "使用D来实现数值处理：" + Thought_process
+        print(Action)
+    def symbol_operations(self, Thought_process):
+        # 使用D来实现符号处理动作
+        Action = "使用D来实现符号处理：" + Thought_process
+        print(Action)
+
+# 创建多模态大模型、大脑和四肢对象
+My_multimodal_models = Multimodal_models()
+My_brain = Brain(My_multimodal_models)
+My_A = A()
+My_B = B()
+My_C = C()
+My_D = D()
+# 根据场景选择A,B,C,D四种情况之一来实现思考过程
+scene = input()
+# 智能体开始工作
+enter = "输入数据"
+# 输入要接受处理的的对象
+Multimodal = input()
+Thought_process = My_brain.ponder(input, Multimodal)
+if scene == '1':
+    if Multimodal == "图片":
+        My_A.picture_action(Thought_process)
+    elif Multimodal == "文本":
+        My_A.text_changes(Thought_process)
+    elif Multimodal == "数值":
+        My_A.value_change(Thought_process)
+    elif Multimodal == "符号":
+        My_A.symbol_operations(Thought_process)
+elif scene == '2':
+    if Multimodal == "图片":
+        My_B.picture_action(Thought_process)
+    elif Multimodal == "文本":
+        My_B.text_changes(Thought_process)
+    elif Multimodal == "数值":
+        My_B.value_change(Thought_process)
+    elif Multimodal == "符号":
+        My_B.symbol_operations(Thought_process)
+if scene == '3':
+    if Multimodal == "图片":
+        My_C.picture_action(Thought_process)
+    elif Multimodal == "文本":
+        My_C.text_changes(Thought_process)
+    elif Multimodal == "数值":
+        My_C.value_change(Thought_process)
+    elif Multimodal == "符号":
+        My_C.symbol_operations(Thought_process)
+if scene == '4':
+    if Multimodal == "图片":
+        My_D.picture_action(Thought_process)
+    elif Multimodal == "文本":
+        My_D.text_changes(Thought_process)
+    elif Multimodal == "数值":
+        My_D.value_change(Thought_process)
+    elif Multimodal == "符号":
+        My_D.symbol_operations(Thought_process)
+
