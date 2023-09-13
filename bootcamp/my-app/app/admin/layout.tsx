5105bf4923334f0394847d634e7c753389af07c7
江纬 <wei@algmon.com>
2023-09-13 20:52:38 +0800

    8 - Programmatic Navigation

--- bootcamp/my-app/app/admin/layout.tsx
+++ bootcamp/my-app/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactNode } from 'react';
 
 interface Props {
     children: ReactNode;
--- bootcamp/my-app/app/users/page.tsx
+++ bootcamp/my-app/app/users/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import UserTable from './UserTable'
 
@@ -11,6 +12,7 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
     return (
         <>
             <h1>Users</h1>
+            <Link href="/users/new" className='btn'>New User</Link>
             <UserTable sortOrder={sortOrder}/>
         </>
     )
