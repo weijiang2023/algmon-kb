a1d35421be97b6cc9bcbcd4b93690c83543ececd
江纬 <wei@algmon.com>
2023-09-14 17:14:50 +0800

    ADD User Operation

---
+++ bootcamp/my-app/app/api/users/[id]/route.tsx
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../scheme";
+import prisma from "@/prisma/client";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: string}}) {
+    const user = await prisma.user.findUnique({
+            where: {id: parseInt(params.id)}
+    });
+
+    if (!user)
+        return NextResponse.json({ error: 'User not found'}, { status: 404});
+    
+    return NextResponse.json(user);
+}
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: string} }){
+    const body = await request.json();
+    const validation = schema.safeParse(body);
+    
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { 
+            status: 400
+        });
+    
+    const user = await prisma.user.findUnique({
+        where: {id: parseInt(params.id) }
+    });
+    
+    if (!user)
+        return NextResponse.json(
+            { error: 'User not found'}, 
+            { status: 404}
+        );
+    
+    const updatedUser = await prisma.user.update({
+        where: { id: user.id },
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+
+    return NextResponse.json(updatedUser)
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: string} }
+) {
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(params.id)}
+    })
+
+    if (!user)
+        return NextResponse.json(
+            { error: 'User not found'}, 
+            { status: 404}
+        );
+
+    await prisma.user.delete({
+        where: { id: user.id }
+    });
+
+    return NextResponse.json({});
+}
---
+++ bootcamp/my-app/app/api/users/route.tsx
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "./scheme";
+import prisma from "@/prisma/client";
+
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany({})
+
+    return NextResponse.json(users);
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = schema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400})
+    
+    const user = await prisma.user.findUnique({
+        where: { email: body.email}
+    })
+    
+    if (user)
+        return NextResponse.json({ error: 'User already exists'}, { status: 400});
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    });
+
+    return NextResponse.json(newUser, { status: 201});
+}
