32146a2f82f60d54d684e2590dae47a671f76472
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2023-09-16 02:49:26 +0800

    READY for production

--- bootcamp/my-app/app/layout.tsx
+++ bootcamp/my-app/app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 import NavBar from './NavBar'
 import AuthProvider from './auth/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500']
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AuthProvider>
           <NavBar />
           <main className='p-5'>
