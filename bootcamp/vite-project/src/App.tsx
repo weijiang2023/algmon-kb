7c71b0db1926274453b011ec6b9c9014f2914b73
江纬 <wei@algmon.com>
2023-09-17 02:03:33 +0800

    Play with a button

---
+++ bootcamp/vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button color="primary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ bootcamp/vite-project/src/components/Alert.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+
+export default Alert;
